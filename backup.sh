50a371f525923712e42ecfce4d7bc2d06892f957
myyerrol <myyerrol@126.com>
2017-03-15 17:16:56 +0800

    Update the scripts

--- backup.sh
+++ backup.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 # Copy configurations to backcp
-cp -puv ~/.bashrc ./
-cp -puv ~/.zshrc  ./
+cp -puv ~/.bashrc    ./
+cp -puv ~/.zshrc     ./
+cp -puv ~/.tmux.conf ./
 
 # Copy directories to backup
 cp -pruv ~/.dir_colors     ./
--- setup.sh
+++ setup.sh
@@ -7,29 +7,38 @@ DIRECTORY=$(pwd)
 cd ~/Documents
 
 if [ ! -d ${TERMINAL_COLOR} ]; then
-    echo "Installing ${TERMINAL_COLOR} ..."
+    echo "Installing the ${TERMINAL_COLOR} ..."
     sudo apt-get install dconf-cli
     git clone https://github.com/Anthony25/${TERMINAL_COLOR}.git
     cd ${TERMINAL_COLOR}
     ./install.sh
 else
-    echo "${TERMINAL_COLOR} has been installed!"
+    echo "Finish the installation of ${TERMINAL_COLOR}!"
 fi
 
 if [ ! -d ~/.oh-my-zsh ]; then
-    echo "Installing oh-my-zsh ..." 
+    echo "Installing the oh-my-zsh ..." 
     sudo apt-get install zsh
     sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 else
-    echo "oh-my-zsh has been installed!"
+    echo "Finish the installation of oh-my-zsh!"
 fi
 
+if [ ! -d fonts ]; then
+    echo "Installing the fonts of powerline ..."
+    git clone https://github.com/powerline/fonts.git
+    cd fonts
+    ./install.sh
+else
+    echo "Finish the installation of fonts!"
+fi
 
 cd ${DIRECTORY}
 
 # Copy configurations to user's home
-#cp -puv .bashrc ~/
-#cp -puv .zshrc  ~/
+#cp -puv .bashrc    ~/
+#cp -puv .zshrc     ~/
+#cp -puv .tmux.conf ~/
 
 # Copy directories to user's home
 #cp -pruv ./.dir_colors    ~/
