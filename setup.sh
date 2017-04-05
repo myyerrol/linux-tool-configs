8f8ae3c40ea46cf1604888834cc02e51996b50e9
myyerrol <myyerrol@126.com>
2017-04-05 16:03:14 +0800

    Add the colors

--- setup.sh
+++ setup.sh
@@ -6,9 +6,30 @@ DIRECTORY=$(pwd)
 
 cd ~/Documents
 
+# Use colors
+if command -v tput >/dev/null 2>&1; then
+    ncolors=$(tput colors)
+fi
+
+if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+    RED="$(tput setaf 1)"
+    GREEN="$(tput setaf 2)"
+    YELLOW="$(tput setaf 3)"
+    BLUE="$(tput setaf 4)"
+    BOLD="$(tput bold)"
+    NORMAL="$(tput sgr0)"
+else
+    RED=""
+    GREEN=""
+    YELLOW=""
+    BLUE=""
+    BOLD=""
+    NORMAL=""
+fi
+
 # Set powerline fonts
 if [ ! -d fonts ]; then
-    echo "Installing the fonts of powerline ..."
+    echo "${BLUE}Installing the fonts of powerline...${NORMAL}"
     wget https://github.com/powerline/fonts/archive/master.zip
     unzip -n master.zip
     rm -rf master.zip
@@ -16,73 +37,87 @@ if [ ! -d fonts ]; then
     cd fonts
     ./install.sh
     cd ../
+    echo "${GREEN}Finish the installation of powerline's fonts!${NORMAL}"
 else
-    echo "Finish the installation of fonts!"
+    echo "${GREEN}Finish the installation of powerline's fonts!${NORMAL}"
 fi
 
 # Set terminal color
 if [ ! -d ${TERMINAL_COLOR} ]; then
-    echo "Installing the ${TERMINAL_COLOR} ..."
+    echo "${BLUE}Installing the ${TERMINAL_COLOR}...${NORMAL}"
     sudo apt-get install dconf-cli
     git clone https://github.com/Anthony25/${TERMINAL_COLOR}.git
     cd ${TERMINAL_COLOR}
     ./install.sh
+    cp -pruv ./.dir_colors/ ~/
     cd ../
+    echo "${GREEN}Finish the installation of terminal's colors!${NORMAL}"
 else
-    echo "Finish the installation of ${TERMINAL_COLOR}!"
+    echo "${GREEN}Finish the installation of terminal's colors!${NORMAL}"
 fi
 
-# Set zsh an oh-my-zsh
-if [ ! -d ~/.oh-my-zsh ]; then
-    echo "Installing the oh-my-zsh ..." 
-    sudo apt-get install zsh
-    sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+# Set atom
+if command -v atom > /dev/null 2>&1; then
+    echo "${GREEN}Atom has been installed!${NORMAL}"
 else
-    echo "Finish the installation of oh-my-zsh!"
-fi
-
-# Set atom and packages
-if [ ! -d ~/.atom ]; then
-    echo "Installing the atom ..."
+    echo "${YELLOW}Atom has not been installed!${NORMAL}"
+    echo "${BLUE}Installing the atom...${NORMAL}"
     sudo add-apt-repository ppa:webupd8team/atom
     sudo apt-get update
     sudo apt-get install atom
-else
-    echo "Finish the installation of atom!"
+    echo "${GREEN}Finish the installation of atom!${NORMAL}"
 fi
+
+cp -pruv ${DIRECTORY}/.atom/packages/ ~/.atom
 
 # Set tmux
-if [ ! -f ~/.tmux.conf ]; then
-    echo "Installing the tmux ..."
-    sudo apt-get install tmux
+if command -v tmux > /dev/null 2>&1; then
+    echo "${GREEN}Tmux has been installed!${NORMAL}"
 else
-    echo "Finish the installation of tmux!"
+    echo "${YELLOW}Tmux has not been installed!${NORMAL}"
+    echo "${BLUE}Installing the tmux...${NORMAL}"
+    sudo apt-get install tmux
+    echo "${GREEN}Finish the installation of tmux!${NORMAL}"
 fi
 
+cp -puv ${DIRECTORY}/.tmux.conf ~/
+
 # Set vim
-if [ ! -f ~/.vimrc ]; then
-    echo "Installing the vim ..."
-    sudo apt-get install vim
+if command -v vim > /dev/null 2>&1; then
+    echo "${GREEN}Vim has been installed!${NORMAL}"
 else
-    echo "Finish the installation of vim!"
+    echo "${YELLOW}Vim has not been installed!${NORMAL}"
+    echo "${BLUE}Installing the vim...${NORMAL}"
+    sudo apt-get install vim
+    echo "${GREEN}Finish the installation of vim!${NORMAL}"
 fi
 
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
-    echo "Installing the vundle ..."
+    echo "${BLUE}Installing the vundle...${NORMAL}"
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    echo "${GREEN}Finish the installation of vundle!${NORMAL}"
 else
-    echo "Finish the installation of vundle!"
+    echo "${GREEN}Finish the installation of vundle!${NORMAL}"
 fi
 
-cd ${DIRECTORY}
+cp -puv ${DIRECTORY}/.vimrc ~/
 
-# Copy configurations to user's home
-cp -puv .zshrc     ~/
-cp -puv .bashrc    ~/
-cp -puv .vimrc     ~/
-cp -puv .tmux.conf ~/
+# Set zsh and oh-my-zsh
+if command -v zsh > /dev/null 2>&1; then
+    echo "${GREEN}ZSH has been installed!${NORMAL}"
+else
+    echo "${YELLOW}ZSH has not been installed!${NORMAL}"
+    echo "${BLUE}Installing the zsh...${NORMAL}"
+    sudo apt-get install zsh
+    echo "${GREEN}Finish the installation of zsh!${NORMAL}"
+fi
 
-# Copy directories to user's home
-cp -pruv ./.dir_colors/    ~/
-cp -pruv ./.atom/packages/ ~/.atom
+if [ ! -d ~/.oh-my-zsh ]; then
+    echo "${BLUE}Installing the oh-my-zsh...${NORMAL}"
+    sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+    echo "${GREEN}Finish the installation of oh-my-zsh!${NORMAL}"
+else
+    echo "${GREEN}Finish the installation of oh-my-zsh!${NORMAL}"
+fi
 
+cp -puv ${DIRECTORY}/.zshrc ~/
