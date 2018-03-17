bc6530f4aa80f72462a0a5ae2e7530db1fc5d968
myyerrol <myyerrol@126.com>
2018-03-17 17:19:59 +0800

    Adjust shell scripts

--- setup.sh
+++ setup.sh
@@ -1,166 +1,76 @@
 #!/usr/bin/env bash
 
-# If abort error, exit shell script
 set -e
 
-# Display some informations
-echo "Author: myyerrol"
-echo "Date: 2017-01-13"
-echo "Desc: This repository contains some software configurations that are used commonly under Linux, such as vim, tmux, zsh, bash, atom, xterm, etc."
-echo ""
+displayVersion()
+{
+    echo "Version:"
+    echo "1.0.0"
+}
 
-DIRECTORY=$(pwd)
+displayUsage()
+{
+    echo "Usage:"
+    echo "./setup.sh [--install | --uninstall]"
+}
 
-# Set the colors
-if command -v tput > /dev/null 2>&1; then
-    ncolors=$(tput colors)
+displayInfo()
+{
+    echo "Author:"
+    echo "myyerrol"
+    echo ""
+    echo "Created on: "
+    echo "2017-01-13"
+}
+
+displayHelp()
+{
+    echo ""
+    displayInfo
+    echo ""
+    displayVersion
+    echo ""
+    displayUsage
+    echo ""
+    echo "Description:"
+    echo "Install or uninstall configuration files that are used commonly under Linux, such as vim, tmux, zsh, bash, xterm, etc. "
+    echo
+    echo "--install       Install configuration files"
+    echo "--uninstall     Uninstall configuration files"
+    echo "--version       Display repository version"
+    echo ""
+    echo "For more information, see https://github.com/myyerrol/linux_user_configuration"
+    echo ""
+}
+
+if [ "$1" = "--debug" ]; then
+    export LUC_DEBUG=1
+    shift
 fi
 
-if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
-    RED="$(tput setaf 1)"
-    GREEN="$(tput setaf 2)"
-    YELLOW="$(tput setaf 3)"
-    BLUE="$(tput setaf 4)"
-    BOLD="$(tput bold)"
-    NORMAL="$(tput sgr0)"
-else
-    RED=""
-    GREEN=""
-    YELLOW=""
-    BLUE=""
-    BOLD=""
-    NORMAL=""
-fi
+[ -n "${LUC_DEBUG}" ] && set -x
+    
+case "$1" in
+    "--version")
+        displayVersion
+        ;;
+    "--usage")
+        displayUsage
+        ;;
+    "--help")
+        displayHelp
+        ;;
+    "--install")
+        bash ./setup_install.sh
+        ;;
+    "--uninstall")
+        bash ./setup_uninstall.sh
+        ;;
+    *)
+        echo "Unknown parameter: "$1"" >&2
+        displayUsage >&2
+        exit 1
+        ;;
+esac
 
-# Set the directory
-if [ ! -d ~/Softwares ]; then
-    mkdir ~/Softwares
-    echo "${GREEN}Creating the directory of softwares successfully!${NORMAL}"
-else
-    echo "${GREEN}The directory of softwares has been created successfully!${NORMAL}"
-cd ~/Softwares
-fi
-
-# Set the powerline's fonts
-if [ ! -d fonts ]; then
-    echo "${BLUE}Downloading the fonts of powerline...${NORMAL}"
-    wget https://github.com/powerline/fonts/archive/master.zip
-    unzip -n master.zip
-    rm -rf master.zip
-    mv fonts-master fonts
-    cd fonts
-    ./install.sh
-    cd ../
-    echo "${GREEN}Setting the powerline's fonts successfully!${NORMAL}"
-else
-    echo "${GREEN}The powerline's fonts have been set successfully!${NORMAL}"
-fi
-
-# Set the terminal's colors
-TERMINAL_COLOR="gnome-terminal-colors-solarized"
-
-if [ ! -d ${TERMINAL_COLOR} ]; then
-    echo "${BLUE}Downloading the ${TERMINAL_COLOR}...${NORMAL}"
-    sudo apt-get install dconf-cli
-    git clone https://github.com/Anthony25/${TERMINAL_COLOR}.git
-    cd ${TERMINAL_COLOR}
-    ./install.sh
-    cp -pruv ./dir_colors/ ~/.dir_colors/
-    # cd ../
-    echo "${GREEN}Setting the terminal's colors successfully!${NORMAL}"
-else
-    echo "${GREEN}The terminal's colors have been set successfully!${NORMAL}"
-fi
-
-# Set xterm and font
-if command -v xterm > /dev/null 2>&1; then
-    echo "${GREEN}Xterm has been installed successfully!${NORMAL}"
-else
-    echo "${YELLOW}Xterm has not been installed!${NORMAL}"
-    echo "${BLUE}Installing the xterm...${NORMAL}"
-    sudo apt-get install xterm
-    echo "${GREEN}Installing the xterm successfully!${NORMAL}"
-fi
-
-if [ -f /usr/share/fonts/truetype/wqy/wqy-microhei.ttc ]; then
-    echo "${GREEN}The wqy-microhei font has been installed successfully!${NORMAL}"
-else
-    echo "${YELLOW}The wqy-microhei font has not been installed!${NORMAL}"
-    echo "${BLUE}install the wqy-microhei font...${NORMAL}"
-    sudo apt-get install ttf-wqy-microhei
-    echo "${GREEN}Installing the wqy-microhei font successfully!${NORMAL}"
-fi
-
-cp -puv ${DIRECTORY}/xterm/.Xresources ~/
-xrdb -load ~/.Xresources
-
-# Set atom
-if command -v atom > /dev/null 2>&1; then
-    echo "${GREEN}Atom has been installed successfully!${NORMAL}"
-else
-    echo "${YELLOW}Atom has not been installed!${NORMAL}"
-    echo "${BLUE}Installing the atom...${NORMAL}"
-    sudo add-apt-repository ppa:webupd8team/atom
-    sudo apt-get update
-    sudo apt-get install atom
-    echo "${GREEN}Installing the atom successfully!${NORMAL}"
-fi
-
-cp -pruv ${DIRECTORY}/atom/packages/   ~/.atom
-cp -puv  ${DIRECTORY}/atom/styles.less ~/.atom
-
-# Set tmux
-if command -v tmux > /dev/null 2>&1; then
-    echo "${GREEN}Tmux has been installed successfully!${NORMAL}"
-else
-    echo "${YELLOW}Tmux has not been installed!${NORMAL}"
-    echo "${BLUE}Installing the tmux...${NORMAL}"
-    sudo apt-get install tmux
-    echo "${GREEN}Installing the tmux successfully!${NORMAL}"
-fi
-
-cp -puv ${DIRECTORY}/tmux/.tmux.conf ~/
-
-# Set vim and vundle
-if command -v vim > /dev/null 2>&1; then
-    echo "${GREEN}Vim has been installed successfully!${NORMAL}"
-else
-    echo "${YELLOW}Vim has not been installed!${NORMAL}"
-    echo "${BLUE}Installing the vim...${NORMAL}"
-    sudo apt-get install vim
-    echo "${GREEN}Installing the vim successfully!${NORMAL}"
-fi
-
-if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
-    echo "${BLUE}Downloading the vundle...${NORMAL}"
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-    echo "${GREEN}Setting the vundle successfully!${NORMAL}"
-else
-    echo "${GREEN}The vundle has been set successfully!${NORMAL}"
-fi
-
-cp -puv ${DIRECTORY}/vim/.vimrc ~/
-
-# Set zsh and oh-my-zsh
-if command -v zsh > /dev/null 2>&1; then
-    echo "${GREEN}ZSH has been installed successfully!${NORMAL}"
-else
-    echo "${YELLOW}ZSH has not been installed!${NORMAL}"
-    echo "${BLUE}Installing the zsh...${NORMAL}"
-    sudo apt-get install zsh
-    echo "${GREEN}Installing the zsh successfully!${NORMAL}"
-fi
-
-if [ ! -d ~/.oh-my-zsh ]; then
-    echo "${BLUE}Downloading the oh-my-zsh...${NORMAL}"
-    sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-    echo "${GREEN}Setting the oh-my-zsh successfully!${NORMAL}"
-else
-    echo "${GREEN}The oh-my-zsh has been set successfully!${NORMAL}"
-fi
-
-cp -puv ${DIRECTORY}/zsh/.zshrc ~/
-
-# Display some informations
-echo ""
-echo "Everything is ok! Start to enjoy it!"
+unset LUC_DEBUG
---
+++ setup_install.sh
@@ -0,0 +1,191 @@
+#!/usr/bin/env bash
+
+set -e
+
+PATH_HOME=${HOME}
+PATH_EXEC=${PWD}
+
+setDisplayedColors()
+{
+    if command -v tput > /dev/null 2>&1; then
+        ncolors=$(tput colors)
+    fi
+
+    if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+        RED="$(tput setaf 1)"
+        GREEN="$(tput setaf 2)"
+        YELLOW="$(tput setaf 3)"
+        BLUE="$(tput setaf 4)"
+        BOLD="$(tput bold)"
+        NORMAL="$(tput sgr0)"
+    else
+        RED=""
+        GREEN=""
+        YELLOW=""
+        BLUE=""
+        BOLD=""
+        NORMAL=""
+    fi
+}
+
+setSoftwaresDirectory()
+{
+    if [ ! -d ${PATH_HOME}/Softwares ]; then
+        mkdir ${PATH_HOME}/Softwares
+        echo "${GREEN}Creating the directory of softwares successfully!${NORMAL}"
+    else
+        echo "${GREEN}The directory of softwares has been created successfully!${NORMAL}"
+    fi
+}
+
+setPownlineFonts()
+{
+    if [ ! -d ${PATH_HOME}/Softwares/fonts ]; then
+        echo "${BLUE}Downloading the fonts of powerline...${NORMAL}"
+        cd ${PATH_HOME}/Softwares
+        wget https://github.com/powerline/fonts/archive/master.zip
+        unzip -n master.zip
+        rm -rf master.zip
+        mv fonts-master fonts
+        cd fonts
+        ./install.sh
+        cd - /dev/null 2>&1
+        echo "${GREEN}Setting the powerline's fonts successfully!${NORMAL}"
+    else
+        echo "${GREEN}The powerline's fonts have been set successfully!${NORMAL}"
+    fi
+}
+
+setTerminalColor()
+{
+    TERMINAL_COLOR="gnome-terminal-colors-solarized"
+
+    if [ ! -d ${PATH_HOME}/Softwares/${TERMINAL_COLOR} ]; then
+        echo "${BLUE}Downloading the ${TERMINAL_COLOR}...${NORMAL}"
+        cd ${PATH_HOME}/Softwares
+        sudo apt-get install dconf-cli
+        git clone https://github.com/Anthony25/${TERMINAL_COLOR}.git
+        cd ${TERMINAL_COLOR}
+        ./install.sh
+        cd - /dev/null 2>&1
+        cp -pruv ./dir_colors/ ${PATH_HOME}/.dir_colors/
+        echo "${GREEN}Setting the terminal's colors successfully!${NORMAL}"
+    else
+        echo "${GREEN}The terminal's colors have been set successfully!${NORMAL}"
+    fi
+}
+
+setXterm()
+{
+    if command -v xterm > /dev/null 2>&1; then
+        echo "${GREEN}Xterm has been installed successfully!${NORMAL}"
+    else
+        echo "${YELLOW}Xterm has not been installed!${NORMAL}"
+        echo "${BLUE}Installing the xterm...${NORMAL}"
+        sudo apt-get install xterm
+        echo "${GREEN}Installing the xterm successfully!${NORMAL}"
+    fi
+
+    if [ -f /usr/share/fonts/truetype/wqy/wqy-microhei.ttc ]; then
+        echo "${GREEN}The wqy-microhei font has been installed successfully!${NORMAL}"
+    else
+        echo "${YELLOW}The wqy-microhei font has not been installed!${NORMAL}"
+        echo "${BLUE}install the wqy-microhei font...${NORMAL}"
+        sudo apt-get install ttf-wqy-microhei
+        echo "${GREEN}Installing the wqy-microhei font successfully!${NORMAL}"
+    fi
+
+    cp -puv ${PATH_EXEC}/xterm/.Xresources ${PATH_HOME}/
+    xrdb -load ${PATH_HOME}/.Xresources
+}
+
+setTmux()
+{
+    if command -v tmux > /dev/null 2>&1; then
+        echo "${GREEN}Tmux has been installed successfully!${NORMAL}"
+    else
+        echo "${YELLOW}Tmux has not been installed!${NORMAL}"
+        echo "${BLUE}Installing the tmux...${NORMAL}"
+        sudo apt-get install tmux
+        echo "${GREEN}Installing the tmux successfully!${NORMAL}"
+    fi
+
+    cp -puv ${PATH_EXEC}/tmux/.tmux.conf ${PATH_HOME}/
+}
+
+setVimAndVundle()
+{
+    if command -v vim > /dev/null 2>&1; then
+        echo "${GREEN}Vim has been installed successfully!${NORMAL}"
+    else
+        echo "${YELLOW}Vim has not been installed!${NORMAL}"
+        echo "${BLUE}Installing the vim...${NORMAL}"
+        sudo apt-get install vim
+        echo "${GREEN}Installing the vim successfully!${NORMAL}"
+    fi
+
+    if [ ! -d ${PATH_HOME}/.vim/bundle/Vundle.vim ]; then
+        echo "${BLUE}Downloading the vundle...${NORMAL}"
+        git clone https://github.com/VundleVim/Vundle.vim.git ${PATH_HOME}/.vim/bundle/Vundle.vim
+        echo "${GREEN}Setting the vundle successfully!${NORMAL}"
+    else
+        echo "${GREEN}The vundle has been set successfully! Updating...${NORMAL}"
+        cd ${PATH_HOME}/.vim/bundle/Vundle.vim
+        git pull
+        cd - /dev/null 2>&1
+    fi
+
+    cp -puv ${PATH_EXEC}/vim/.vimrc ${PATH_HOME}/
+
+    echo "${BLUE}Installing vim plugins...${BLUE}"
+    vim +PluginInstall +qall
+}
+
+setZSH()
+{
+    if command -v zsh > /dev/null 2>&1; then
+        echo "${GREEN}ZSH has been installed successfully!${NORMAL}"
+    else
+        echo "${YELLOW}ZSH has not been installed!${NORMAL}"
+        echo "${BLUE}Installing the zsh...${NORMAL}"
+        sudo apt-get install zsh
+        echo "${GREEN}Installing the zsh successfully!${NORMAL}"
+    fi
+
+    chsh -s /bin/zsh
+
+    cp -ipv ${PATH_EXEC}/zsh/.zshrc ${PATH_HOME}/
+
+    if [ ! -d ${PATH_HOME}/.oh-my-zsh ]; then
+        echo "${BLUE}Downloading the oh-my-zsh...${NORMAL}"
+        sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+        echo "${GREEN}Setting the oh-my-zsh successfully!${NORMAL}"
+    else
+        echo "${GREEN}The oh-my-zsh has been set successfully!${NORMAL}"
+    fi
+}
+
+displayCompletedInfo()
+{
+    echo ""
+    echo "Everything is ok! Start to enjoy it!"
+}
+
+main()
+{
+    setDisplayedColors
+    setSoftwaresDirectory
+
+    setPownlineFonts
+    setTerminalColor
+    setXterm
+    setTmux
+    setVimAndVundle
+    setZSH
+    
+    displayCompletedInfo
+}
+
+# Run script
+RUNNING=$(basename $0)
+[ "$RUNNING" = "setup_install.sh" ] && main
